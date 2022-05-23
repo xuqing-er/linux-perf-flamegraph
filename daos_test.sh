9dbde378fa7f8a096723b4382a5cd9c7935c9547
xuqinger <duanshibo.d@gmail.com>
2022-05-23 06:11:13 +0000

    pid

--- daos_test.sh
+++ daos_test.sh
@@ -1,11 +1,13 @@
-if [ $# != 2 ]
+if [ $# != 3 ]
 then
-    echo 'must specify client_thread and server thread'
+    echo 'must specify client_thread, server thread num, pid'
     exit -1
 fi
 
 client_pthread_num=$1
 server_pthread_num=$2
+test_pid=$3
+
 Flame_bin_path=${HOME}/FlameGraph
 test_path=${HOME}/daos_flame
 sub_path=${HOME}/daos_flame/${client_pthread_num}_${server_pthread_num}
@@ -24,7 +26,7 @@ perf_file_path=${sub_path}/test.perf
 folded_file_path=${sub_path}/test.folded
 svg_file_path=${sub_path}/test.svg
 
-sudo perf record -F 99 -g -p 1002452 -- sleep 40
+sudo perf record -F 99 -g -p ${test_pid} -- sleep 40
 sudo perf script > ${perf_file_path}
 ${Flame_bin_path}/stackcollapse-perf.pl ${perf_file_path} > ${folded_file_path}
 ${Flame_bin_path}/flamegraph.pl ${folded_file_path} > ${svg_file_path}
